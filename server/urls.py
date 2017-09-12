0eb2b7a9d39f68ac2a3970d2503f6c3b82d3dd59
niteenautade <niteenautade@gmail.com>
2017-09-12 23:59:17 +0530

    static files served

--- server/urls.py
+++ server/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', serve, kwargs={'path': 'index.html'}),
+    url(r'^(?!/?static/)(?!/?media/)(?P<path>.*\..*)$',
+    RedirectView.as_view(url='/static/%(path)s', permanent=False)),
 ]
